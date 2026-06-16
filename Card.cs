3ad866ebc840eb2de90ece70da546956764bf4fd
Web Development <rmfredm@stu.ccsdut.org>
2026-06-16 11:18:37 -0600

    Updated the Card.RankString() method
    RankString() now has a default value of "Ace".

--- Card.cs
+++ Card.cs
@@ -66,7 +66,7 @@ namespace GameOfWar
                     return "Queen";
                 case 11:
                     return "King";
-                case 12:
+                default:
                     return "Ace";
             }
         }
