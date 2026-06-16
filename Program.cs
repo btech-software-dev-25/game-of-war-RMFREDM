bd8573fdfcd9d3935cb77326ead51d798cbcb006
Web Development <rmfredm@stu.ccsdut.org>
2026-06-16 13:19:08 -0600

    Completed the PlayCards() function
    PlayCards() now pulls and compares the appropriate cards from the PlayerDeck and ComputerDeck and performs the appropriate action. PlayCards() also accurately determines if there is a winner to the game.

--- Program.cs
+++ Program.cs
@@ -21,8 +21,30 @@ state.ComputerDeck.PushCards(state.CardDeck.Deal(26));
 //         If the player deck is empty, the computer wins and state.Winner should be set to "Player"
 //     return true
 static bool PlayCards(GameState state, int playerCardIndex) {
+    // pull cards from the player and computer decks
+    Card playerCard = state.PlayerDeck.PullCardAtIndex(playerCardIndex);
+    Card computerCard = state.ComputerDeck.PullCardAtIndex(0);
 
+    // compare the cards and perform the appropriate action
+    if (playerCard > computerCard) {
+        state.PlayerDeck.PushCard(computerCard);
+        state.PlayerDeck.PushCards(state.TableDeck.PullAllCards());
+    } else if (playerCard < computerCard) {
+        state.ComputerDeck.PushCard(playerCard);
+        state.ComputerDeck.PushCards(state.TableDeck.PullAllCards());
+    } else {
+        state.TableDeck.PushCards([playerCard, computerCard]);
+    }
+
+    // check if there is a winner
+    if (state.ComputerDeck.Count == 0) {
+        state.Winner = "Player";
+    } else if (state.PlayerDeck.Count == 0) {
+        state.Winner = "Computer";
+    }
+
+    return true;
 }
 
 // Call Lib.RunGame(), passing two parameters: the state object you instantiated above and the name of your PlayCards function
-// Lib.RunGame(state, PlayCards);
+Lib.RunGame(state, PlayCards);
