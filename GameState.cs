940f0692737bf77defad5493df12dfc48c979f6b
Web Development <rmfredm@stu.ccsdut.org>
2026-06-16 10:07:02 -0600

    Moved the GameState class to its own file
    The GameState Class now resides in GameState.cs and not at the bottom of Program.cs

---
+++ GameState.cs
@@ -0,0 +1,25 @@
+namespace GameOfWar
+{
+    public class GameState
+    {
+        // Create a public Deck property called CardDeck
+
+
+        // Create a public Deck property called PlayerDeck
+
+
+        // Create a public Deck property called ComputerDeck
+
+
+        // Create a public Deck property called TableDeck
+
+
+        // Create a public string property called Winner
+
+
+        // Create a public constructor that accepts no parameters. It should:
+        //    Initialize Winner to be empty (not null)
+        //    Initialize CardDeck to be a new, fresh deck of 52 cards
+        //    Initialize PlayerDeck, ComputerDeck, and TableDeck to be empty Deck objects (no cards)
+    }
+}
--- Program.cs
+++ Program.cs
@@ -20,29 +20,3 @@
 
 
 // Call Lib.RunGame(), passing two parameters: the state object you instantiated above and the name of your PlayCards function
-
-namespace GameOfWar
-{
-    public class GameState
-    {
-        // Create a public Deck property called CardDeck
-
-
-        // Create a public Deck property called PlayerDeck
-
-
-        // Create a public Deck property called ComputerDeck
-
-
-        // Create a public Deck property called TableDeck
-
-
-        // Create a public string property called Winner
-
-
-        // Create a public constructor that accepts no parameters. It should:
-        //    Initialize Winner to be empty (not null)
-        //    Initialize CardDeck to be a new, fresh deck of 52 cards
-        //    Initialize PlayerDeck, ComputerDeck, and TableDeck to be empty Deck objects (no cards)
-    }
-}
